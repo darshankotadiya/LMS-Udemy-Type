fdef9a94a5cfe42ae19ef4223b819914a1175e27
Darshan Kotadiya <darshankotadiya1010@gmail.com>
2026-03-16 10:53:16 +0530

    fix cors for vercel domain

--- server/app.js
+++ server/app.js
@@ -27,8 +27,7 @@ app.use(morgan('dev'));
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'https://advanced-lms.vercel.app' 
-  ],
+ "https://lms-udemy-type.vercel.app"  ],
   credentials: true
 }));
 
