82a1a3d2e93f7f6963aec117a2e86a5df3f6be5f
Darshan Kotadiya <darshankotadiya1010@gmail.com>
2026-03-16 11:17:45 +0530

    final cors fix for production

--- server/app.js
+++ server/app.js
@@ -25,9 +25,7 @@ app.use(morgan('dev'));
 
 // CORS configuration
 app.use(cors({
-  origin: [
-    'http://localhost:5173', 
- "https://lms-udemy-type.vercel.app"  ],
+  origin: true,
   credentials: true
 }));
 
